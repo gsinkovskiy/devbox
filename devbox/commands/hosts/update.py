0626fdf5564496418b5a9d9e06229a343b14d62e
Konstantin Myakshin <koc-dp@yandex.ru>
2017-11-10 01:37:50 +0200

    Update hosts with sudo

--- devbox/commands/hosts/update.py
+++ devbox/commands/hosts/update.py
@@ -1,16 +1,18 @@
 import click
 
+
 @click.group(name='hosts')
 def commands():
     pass
 
+
 @commands.command(name='hosts:update')
 @click.argument('container', nargs=-1)
-def execute(container = None):
+def execute(container=None):
     """
     Update /etc/hosts file depends on current running containers
     """
-    #print(container)
+    # print(container)
     click.echo('Update hosts')
 
     from devbox.utils import admin, docker as docker_utils
@@ -19,24 +21,26 @@ def execute(container = None):
     from python_hosts.exception import UnableToWriteHosts
 
     #f = open('C:\\apps\\devbox\\a.txt', 'a')
-    #f.write('1')
-    #f.close()
+    # f.write('1')
+    # f.close()
 
     client = docker.from_env()
-    #TODO: support containers
+    # TODO: support containers
     containers = client.containers.list()
     hosts = Hosts()
     require_update = False
 
     for container in containers:
-        #print(container.name)
+        # print(container.name)
         container_hosts = docker_utils.get_hosts(container)
         ip = docker_utils.get_ip(container)
 
         if (container_hosts):
             for container_host in container_hosts:
-                hosts_entry = HostsEntry(entry_type='ipv4', address=ip, names=[container_host])
-                hosts.add([hosts_entry], force=True, allow_address_duplication=True)
+                hosts_entry = HostsEntry(
+                    entry_type='ipv4', address=ip, names=[container_host])
+                hosts.add([hosts_entry], force=True,
+                          allow_address_duplication=True)
             require_update = True
 
     if (not require_update):
@@ -46,10 +50,18 @@ def execute(container = None):
     try:
         hosts.write()
     except UnableToWriteHosts:
-        if (not admin.is_admin()):
-            click.echo('Unable update hosts file, retry again with admin roots.')
-            admin.run_as_admin('devbox', ('hosts:update'))
+        from sys import platform
+
+        if platform.startswith('win'):
+            if (not admin.is_admin()):
+                click.echo('Unable update hosts file, retry again with admin roots.')
+                admin.run_as_admin('devbox', ('hosts:update'))
+
             return
+
+        from subprocess import call
+        click.echo('Unable update hosts file, retry again with sudo.')
+        call('sudo devbox hosts:update')
 
     hosts.write()
     click.echo('Done')
